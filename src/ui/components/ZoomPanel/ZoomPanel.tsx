019be595f967c0cff9e940459a1900eccba17495
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-05-01 15:55:27 +0300

    add zoom control

--- src/ui/components/ZoomPanel/ZoomPanel.tsx
+++ src/ui/components/ZoomPanel/ZoomPanel.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import { ReactZoomPanPinchContentRef } from 'react-zoom-pan-pinch';
-import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import { useControls } from 'react-zoom-pan-pinch';
+import FilterCenterFocusIcon from '@mui/icons-material/FilterCenterFocus';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import { Button, Slider } from '@mui/material';
+import { DEFAULT_SCALE } from 'ui/hocs/withZooming';
 
 import styles from './ZoomPanel.module.scss';
 
-export const ZoomPanel = ({ zoomIn, zoomOut, centerView }:
-  ReactZoomPanPinchContentRef) => {
+export const ZoomPanel = () => {
+  const { resetTransform, centerView, setTransform, instance } = useControls();
+  const [scale, setScale] = useState(DEFAULT_SCALE);
+
+  const [zoomInstance] = useState(instance);
+
+  const zoom = (value: number) => {
+
+    setScale(value);
+
+    setTransform(
+      zoomInstance.props.initialPositionX || 0,
+      zoomInstance.props.initialPositionY || 0,
+      value,
+    );
+  };
+
+  const resetZoom = () => {
+    setScale(DEFAULT_SCALE);
+    resetTransform();
+  };
+
   return <div className={styles.zoomPanel}>
-    <Button onClick={() => zoomIn()}>Приблизить</Button>
-    <Button onClick={() => zoomOut()}>Уменьшить</Button>
-    <Button onClick={() => centerView()}>Сбросить</Button>
+    <div>
+      <Button
+        className={styles.centerBtn}
+        onClick={() => centerView()}
+      >
+        <FilterCenterFocusIcon />
+      </Button>
+      <Button
+        className={styles.centerBtn}
+        onClick={resetZoom}
+      >
+        <RestartAltIcon />
+      Сбросить
+      </Button>
+    </div>
+    <div className={styles.sliderZoom}>
+      <Slider
+        aria-label="Zoom"
+        orientation="vertical"
+        defaultValue={zoomInstance.props.initialScale}
+        valueLabelDisplay="auto"
+        value={scale}
+        onChange={(e, v) => zoom(v as number)}
+        min={0}
+        max={2}
+        step={0.1}
+      />
+    </div>
+
   </div>;
 };
--- src/ui/hocs/withZooming.tsx
+++ src/ui/hocs/withZooming.tsx
@@ -4,20 +4,19 @@ import { ZoomPanel } from 'ui/components/ZoomPanel';
 
 import styles from './withZooming.module.scss';
 
-const DEFAULT_SCALE = {
-  initialScale: 1.1,
-};
+export const DEFAULT_SCALE = 1;
 
 export const withZooming = <P extends object>(Component: ComponentType<P>) => {
   return function ZoomedComponent(props: P) {
 
-    return <TransformWrapper {...DEFAULT_SCALE} centerOnInit>
-      {(zoomProps) => <>
-        <ZoomPanel {...zoomProps} />
-        <TransformComponent wrapperClass={styles.zoomComponent}>
-          <Component {...props} />
-        </TransformComponent></>
-      }
+    return <TransformWrapper initialScale={DEFAULT_SCALE}>
+      <ZoomPanel/>
+      <TransformComponent
+        wrapperClass={styles.zoomWrapper}
+        contentClass={styles.zoomComponent}
+      >
+        <Component {...props} />
+      </TransformComponent>
     </TransformWrapper>;
   }
   ;
