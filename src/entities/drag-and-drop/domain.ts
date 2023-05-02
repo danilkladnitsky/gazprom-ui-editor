94ae69e80890433507f0f56229d04e9d9f02b03c
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-05-02 18:21:35 +0300

    remove entities

--- src/entities/drag-and-drop/domain.ts
+++
@@ -1,9 +0,0 @@
-export type DragAndDropAlias = 'app-form' | 'form-list' | 'component-list';
-
-export type DragItem<Item> = {
-  alias: DragAndDropAlias;
-  item: Item;
-};
-
-export type OnDragFn<Item> = (item: DragItem<Item>) => void;
-export type OnDropFn<Item> = (item: DragItem<Item>) => void;
